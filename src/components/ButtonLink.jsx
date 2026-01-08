5be39f2d4ebf150a3f232cb55af8bdbdfd7b6b4b
Shah-Faisal-cloud <fjcreater99@gmail.com>
2026-01-08 11:00:48 +0500

    bugs fixed

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -3,9 +3,9 @@ import { motion } from "framer-motion"
 
 const MotionLink = motion.create(Link);
 
-const ButtonLink = ({ text, route, className = '' }) => {
+const ButtonLink = ({ text, route, className = '', onClick= () => {} }) => {
     return (
-        <MotionLink to={route} className={`flex justify-center bg-green-deep text-white py-3 font-bold hover:bg-green-muted transition-colors duration-200 ease-linear ${className}`}
+        <MotionLink to={route} onClick={onClick} className={`flex justify-center bg-green-deep text-white py-3 font-bold hover:bg-green-muted transition-colors duration-200 ease-linear ${className}`}
             variants={{
                 hidden: { opacity: 0, y: 30 },
                 visible: { opacity: 1, y: 0 }
